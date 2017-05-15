d9267d1bedd5962106c00fb67f65a067037effff
Bernard Silla Mulobi <bernard.mulobi@andela.com>
2017-05-15 22:42:01 +0300

    fix tests

--- wger/core/tests/test_registration.py
+++ wger/core/tests/test_registration.py
@@ -58,8 +58,8 @@ class RegistrationTestCase(WorkoutManagerTestCase):
 
         # Fill in the registration form
         registration_data = {'username': 'myusername',
-                             'password1': 'secret',
-                             'password2': 'secret',
+                             'password1': 'NhJ8&876&&nhfgs',
+                             'password2': 'NhJ8&876&&nhfgs',
                              'email': 'not an email',
                              'g-recaptcha-response': 'PASSED', }
         count_before = User.objects.count()
@@ -108,8 +108,8 @@ class RegistrationTestCase(WorkoutManagerTestCase):
 
             # Fill in the registration form
             registration_data = {'username': 'myusername',
-                                 'password1': 'secret',
-                                 'password2': 'secret',
+                                 'password1': 'NhJ8&876&&nhfgs',
+                                 'password2': 'NhJ8&876&&nhfgs',
                                  'email': 'my.email@example.com',
                                  'g-recaptcha-response': 'PASSED', }
             count_before = User.objects.count()
